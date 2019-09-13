5a22b37c4458530c4c6ee63d26a42089e0d56c3a
Eric <eric@doranco.fr>
2019-09-13 15:06:45 +0200

    Version initiale

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<title>Projet Web</title>
+	</head>
+	<body>
+		<h1>Projet Web</h1>
+		<form class="form-horizontal">
+		<fieldset>
+		<legend>Interface d'authentification</legend>
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="login">Identifiant</label>  
+		  <div class="col-md-4">
+		  <input name="login" class="form-control input-md" id="login" required="" type="text" placeholder="Votre identifiant de session">
+		  <span class="help-block">Votre e-mail</span>  
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="password">Mot de passe</label>
+		  <div class="col-md-4">
+			<input name="password" class="form-control input-md" id="password" required="" type="password" placeholder="Votre mot de passe">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="valid"></label>
+		  <div class="col-md-4">
+			<button name="valid" class="btn btn-primary" id="valid">S'authentifier</button>
+		  </div>
+		</div>
+		</fieldset>
+		</form>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+</html>
